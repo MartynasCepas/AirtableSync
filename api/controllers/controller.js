6671815ce9533247b17855cede6a22a5af4c472d
MartynasCepas <martynas.cepas@ktu.edu>
2021-04-14 12:38:50 +0300

    Working requests

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -3,6 +3,8 @@ const https = require("https");
 var request = require("request");
 
 const bubbleURL = "https://paper-pusher.bubbleapps.io/version-test/api/1.1/obj";
+const bubbleWorkflowURL =
+  "https://paper-pusher.bubbleapps.io/version-test/api/1.1/wf";
 const airtableURL =
   "https://api.airtable.com/v0/appcu1qWsme6tzNri/Questions?maxRecords=3&view=All";
 
@@ -23,19 +25,47 @@ exports.getDataFromBubble = function (req, res) {
   );
 };
 
-exports.postDataToBubble = function (req, res){
+exports.postDataToBubble = function (req, res) {
   request.post(
     {
       url: bubbleURL + "/TestTable",
       headers: {
         Bearer: "97a592b87f2da8c128ffc1e59af0fc6a",
+        "content-type": "application/json",
       },
+
+      body: JSON.stringify({ test_text: "testapipost15" }),
+
       method: "POST",
     },
 
     function (e, r, body) {
       console.log(body);
+      // console.log(r);
+      res.send(r);
+    }
+  );
+};
+
+exports.deleteDataFromBubble = function (req, res) {
+  request.post(
+    {
+      url:
+        "https://paper-pusher.bubbleapps.io/version-test/api/1.1/wf/delete_thing/initialize",
+      headers: {
+        Bearer: "97a592b87f2da8c128ffc1e59af0fc6a",
+        "content-type": "application/json",
+      },
+
+      body: JSON.stringify({ test_text: "test1" }),
+
+      method: "POST",
+    },
+
+    function (e, r, body) {
+      console.log(body);
+      // console.log(r);
       res.send(body);
     }
   );
-}
+};
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -5,4 +5,6 @@ var controller = require("../controllers/controller");
 module.exports = function (app) {
   app.route("/api/bubble").get(controller.getDataFromBubble);
   app.route("/api/airtable").get(airtableapi.getDataFromAirtable);
+  app.route("/api/bubblepost").get(controller.postDataToBubble);
+  app.route("/api/bubbledelete").get(controller.deleteDataFromBubble);
 };
